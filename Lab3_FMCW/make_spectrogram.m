5f1860287e405727a72269f802c94442bb1230c0
NortheLo <louis@northe-online.de>
2025-05-08 21:44:40 +0200

    added 4.1 and 4.3

--- Lab3_FMCW/make_spectrogram.m
+++ Lab3_FMCW/make_spectrogram.m
@@ -1,34 +1,20 @@
 function [S, F, T] = make_spectrogram(buffer, f_sampling, chunk_size, overlap)
-    % make_spectrogram - Computes the spectrogram of a signal buffer
-    %
-    % Inputs:
-    %   buffer       - Time-domain signal vector
-    %   f_sampling   - Sampling frequency in Hz
-    %   chunk_size   - Window size (FFT length)
-    %   overlap      - Overlap between segments (in samples)
-    %
-    % Outputs:
-    %   S - Spectrogram matrix (complex)
-    %   F - Frequency vector (Hz)
-    %   T - Time vector (s)
+    % out
+    %   S - Spectrogram
+    %   F - Frequency vector 
+    %   T - Time vector
 
-    % Default chunk size if not provided
     if nargin < 3
         chunk_size = 1024;
     end
 
-    % Default overlap if not provided (50%)
     if nargin < 4
         overlap = round(chunk_size / 2);
     end
 
-    % Apply windowing (Hann window)
     win = blackmanharris(chunk_size);
-
-    % Compute spectrogram
     [S, F, T] = spectrogram(buffer, win, overlap, chunk_size, f_sampling);
 
-    % Optional: Plot the spectrogram
     figure;
     imagesc(T, F, 10*log10(abs(S)));
     axis xy;
